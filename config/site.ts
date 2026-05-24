704e7e32b17a42cd2c40c08ef3bf75d5f89c1870
RifqiArdian09 <Kambingjawa009@gmail.com>
2026-05-24 20:29:22 +0700

    chore: update site URL and reset social media links in site configuration

--- config/site.ts
+++ config/site.ts
@@ -2,12 +2,12 @@ export const siteConfig = {
   name: 'SisaRasa',
   description:
     'Selamatkan Makanan, Hemat Pengeluaran. Temukan makanan berkualitas dari UMKM lokal dengan harga lebih hemat sebelum terbuang.',
-  url: process.env.NEXT_PUBLIC_SITE_URL || 'https://sisarasa.vercel.app',
+  url: process.env.NEXT_PUBLIC_SITE_URL || 'https://sisarasa-beige.vercel.app',
   ogImage: '/images/og-image.png',
   links: {
-    instagram: 'https://instagram.com/sisarasa',
-    twitter: 'https://twitter.com/sisarasa',
-    github: 'https://github.com/sisarasa',
+    instagram: '#`',
+    twitter: '#`',
+    github: '#`',
   },
 };
 
